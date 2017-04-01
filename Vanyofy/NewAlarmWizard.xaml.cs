13f2b117f77814094b7ce8ec6e79f197efc48dfa
ivans <ivans@dynamosoftware.com>
2017-04-01 11:19:00 +0300

    separate xaml styles, init alarm wizard

---
+++ Vanyofy/NewAlarmWizard.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vanyofy
+{
+    /// <summary>
+    /// Interaction logic for NewAlarmWizard.xaml
+    /// </summary>
+    public partial class NewAlarmWizard : UserControl
+    {
+        public NewAlarmWizard()
+        {
+            InitializeComponent();
+        }
+    }
+}
