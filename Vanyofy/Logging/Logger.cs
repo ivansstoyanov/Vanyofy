77e094b7acaf875a1f9aa5d759065997f3f73309
ivansstoyanov <ivan.stoyanov.stoyanov@gmail.com>
2017-04-08 20:43:26 +0300

    configure logger programatically

--- Vanyofy/Logging/Logger.cs
+++ Vanyofy/Logging/Logger.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Xml;
 
 namespace Vanyofy.Logging
 {
@@ -12,6 +8,21 @@ namespace Vanyofy.Logging
         
         private static log4net.ILog CreateLogger()
         {
+            XmlDocument doc = new XmlDocument();
+            string xml = "<log4net><root><level value=\"ALL\" />" +
+                "<appender-ref ref=\"MyFileAppender\"/>" +
+                "</root>" +
+                "<appender name=\"MyFileAppender\" type=\"log4net.Appender.FileAppender\" >" +
+                "<file value=\"${LOCALAPPDATA}\\Vanyofy\\Logs\\application.log\" />" +
+                "<appendToFile value=\"true\" />" +
+                "<lockingModel type=\"log4net.Appender.FileAppender+MinimalLock\" />" +
+                "<layout type=\"log4net.Layout.PatternLayout\">" +
+                "<conversionPattern value=\"%date - %message%newline\" />" +
+                "</layout>" +
+                "</appender></log4net>";
+            doc.LoadXml(xml);
+
+            log4net.Config.XmlConfigurator.Configure(doc.DocumentElement);
             return log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         }
     }
